c837f1c2035d6cead8fbf46dd014074e350b327e
Desooe-pro <sacha.allardin06@gmail.com>
2025-10-22 10:37:41 +0200

    Création PWA

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,38 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      injectRegister: "auto",
+      workbox: {
+        globPatterns: ["**/*.{js,jsx,css,html,ico,png,svg}"],
+      },
+      manifest: {
+        name: "Vite PWA test",
+        short_name: "Vite PWA",
+        description: "Test de création d'une PWA avec Vite",
+        theme_color: "#242424",
+        icons: [
+          {
+            src: "vite.svg",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "vite.svg",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
